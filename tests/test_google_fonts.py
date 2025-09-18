ca05916d186964e244bd24e915dfaf63bb7e01ab
fjktkm <fjktkm@gmail.com>
2025-09-18 09:38:03 +0000

    Add dataset modules

---
+++ tests/test_google_fonts.py
@@ -0,0 +1,11 @@
+from torchfont.datasets import GoogleFonts
+
+dataset = GoogleFonts(
+    root="data/google_fonts",
+    ref="main",
+    download=True,
+)
+
+print(f"{len(dataset)=}")
+print(f"{dataset.num_content_classes=}")
+print(f"{dataset.num_style_classes=}")
